d6f731d1b0215e82b42585069d9a462bd61eafac
Jacques Nadeau <jacques@apache.org>
2014-08-28 22:58:31 -0700

    DRILL-1340: Add support for JDBC metadata.

--- exec/jdbc/src/test/java/org/apache/drill/jdbc/test/JdbcTestQueryBase.java
+++ exec/jdbc/src/test/java/org/apache/drill/jdbc/test/JdbcTestQueryBase.java
@@ -17,24 +17,28 @@
  */
 package org.apache.drill.jdbc.test;
 
-import com.google.common.base.Stopwatch;
-import org.apache.commons.io.FileUtils;
-import org.apache.drill.common.util.TestTools;
-import org.apache.drill.exec.store.hive.HiveTestDataGenerator;
-import org.apache.drill.jdbc.Driver;
-import org.apache.drill.jdbc.JdbcTest;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.rules.TestRule;
-
-import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.drill.common.util.TestTools;
+import org.apache.drill.jdbc.Driver;
+import org.apache.drill.jdbc.JdbcTest;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+import com.google.common.base.Stopwatch;
 
 public class JdbcTestQueryBase extends JdbcTest {
   // Set a timeout unless we're debugging.
@@ -42,45 +46,91 @@ public class JdbcTestQueryBase extends JdbcTest {
   public TestRule TIMEOUT = TestTools.getTimeoutRule(40000);
 
   protected static final String WORKING_PATH;
-  static{
+  static {
     Driver.load();
     WORKING_PATH = Paths.get("").toAbsolutePath().toString();
 
   }
 
-  protected void testQuery(String sql) throws Exception{
-    boolean success = false;
-    try (Connection c = DriverManager.getConnection("jdbc:drill:zk=local", null);) {
-      for (int x = 0; x < 1; x++) {
-        Stopwatch watch = new Stopwatch().start();
+  protected void testQuery(final String sql) throws Exception {
+    testAction(new JdbcAction() {
+
+      @Override
+      public ResultSet getResult(Connection c) throws SQLException {
         Statement s = c.createStatement();
         ResultSet r = s.executeQuery(sql);
-        boolean first = true;
-        while (r.next()) {
-          ResultSetMetaData md = r.getMetaData();
-          if (first == true) {
-            for (int i = 1; i <= md.getColumnCount(); i++) {
-              System.out.print(md.getColumnName(i));
-              System.out.print('\t');
-            }
-            System.out.println();
-            first = false;
-          }
-
-          for (int i = 1; i <= md.getColumnCount(); i++) {
-            System.out.print(r.getObject(i));
-            System.out.print('\t');
-          }
-          System.out.println();
-        }
-
-        System.out.println(String.format("Query completed in %d millis.", watch.elapsed(TimeUnit.MILLISECONDS)));
+        return r;
       }
 
-      System.out.println("\n\n\n");
-      success = true;
-    }finally{
-      if(!success) Thread.sleep(2000);
-    }
+    });
   }
+
+  protected void testAction(JdbcAction action) throws Exception {
+    testAction(action, -1);
+  }
+
+  protected void testAction(JdbcAction action, long rowcount) throws Exception {
+    int rows = 0;
+    Stopwatch watch = new Stopwatch().start();
+    ResultSet r = action.getResult(connection);
+    boolean first = true;
+    while (r.next()) {
+      rows++;
+      ResultSetMetaData md = r.getMetaData();
+      if (first == true) {
+        for (int i = 1; i <= md.getColumnCount(); i++) {
+          System.out.print(md.getColumnName(i));
+          System.out.print('\t');
+        }
+        System.out.println();
+        first = false;
+      }
+
+      for (int i = 1; i <= md.getColumnCount(); i++) {
+        System.out.print(r.getObject(i));
+        System.out.print('\t');
+      }
+      System.out.println();
+    }
+
+    System.out.println(String.format("Query completed in %d millis.", watch.elapsed(TimeUnit.MILLISECONDS)));
+
+    if (rowcount != -1)
+      Assert.assertEquals((long) rowcount, (long) rows);
+
+    System.out.println("\n\n\n");
+
+  }
+
+  public static interface JdbcAction {
+    ResultSet getResult(Connection c) throws SQLException;
+  }
+
+  static void resetConnection() throws Exception {
+    closeClient();
+    openClient();
+  }
+
+  static Connection connection;
+
+  @BeforeClass
+  public static void openClient() throws Exception {
+    connection = DriverManager.getConnection("jdbc:drill:zk=local", null);
+  }
+
+  @AfterClass
+  public static void closeClient() throws IOException, SQLException {
+    connection.close();
+  }
+
+  public final TestRule resetWatcher = new TestWatcher() {
+    @Override
+    protected void failed(Throwable e, Description description) {
+      try {
+        resetConnection();
+      } catch (Exception e1) {
+        throw new RuntimeException("Failure while resetting client.", e1);
+      }
+    }
+  };
 }
