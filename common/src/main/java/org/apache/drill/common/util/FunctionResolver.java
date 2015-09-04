e66654cf02f537dbb48aa5b8b975efc9675f89cc
Julien Le Dem <julien@ledem.net>
2015-09-04 16:31:46 -0700

    cleanup

--- common/src/main/java/org/apache/drill/common/util/FunctionResolver.java
+++ common/src/main/java/org/apache/drill/common/util/FunctionResolver.java
@@ -1,23 +1,30 @@
 package org.apache.drill.common.util;
 
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.drill.common.config.CommonConstants;
 import org.reflections.Reflections;
-import org.reflections.Store;
 import org.reflections.adapters.JavassistAdapter;
 import org.reflections.scanners.AbstractScanner;
-import org.reflections.serializers.XmlSerializer;
 import org.reflections.util.ConfigurationBuilder;
 
-import com.google.common.collect.Multimap;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.ClassFile;
@@ -25,21 +32,20 @@ import javassist.bytecode.FieldInfo;
 
 public final class FunctionResolver {
   private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(FunctionResolver.class);
+  private static final String FUNCTION_REGISTRY_FILE = "META-INF/function-registry/registry.json";
 
-  private static final FunctionResolver.FunctionScanner FUNCTION_SCANNER = new FunctionResolver.FunctionScanner();
-  // TODO: make JSON but this require GSON on the cp
-  private static final XmlSerializer REFLECTIONS_SERIALIZER = new XmlSerializer();
-  private static final String REFLECTION_FILE = "org/apache/drill/reflections.xml";
+  private static final ObjectMapper mapper = new ObjectMapper().enable(INDENT_OUTPUT);
+  private static final ObjectReader reader = mapper.reader(ScanResult.class);
+  private static final ObjectWriter writer = mapper.writerWithType(ScanResult.class);
 
   static final class FunctionScanner extends AbstractScanner {
     private String FUNCTION_ANN = "org.apache.drill.exec.expr.annotations.FunctionTemplate";
+    public List<FunctionDescriptor> functions = new ArrayList<>();
 
     @Override
     public void scan(final Object cls) {
       try {
         ClassFile classFile = (ClassFile)cls;
-        Multimap<String, String> store = getStore();
-        FunctionStorage functionStorage = new FunctionStorage();
 
         AnnotationsAttribute annotations = ((AnnotationsAttribute)classFile.getAttribute(AnnotationsAttribute.visibleTag));
         if (annotations != null) {
@@ -59,7 +65,7 @@ public final class FunctionResolver {
               AnnotationsAttribute fieldAnnotations = ((AnnotationsAttribute)field.getAttribute(AnnotationsAttribute.visibleTag));
               fieldDescriptors.add(new FieldDescriptor(fieldName, field.getDescriptor(), getAnnotationDescriptors(fieldAnnotations)));
             }
-            functionStorage.save(null, store, new FunctionDescriptor(classFile.getName(), classAnnotations, fieldDescriptors));
+            functions.add(new FunctionDescriptor(classFile.getName(), classAnnotations, fieldDescriptors));
           }
         }
       } catch (RuntimeException e) {
@@ -87,205 +93,51 @@ public final class FunctionResolver {
     }
   }
 
-
-  public static abstract class Storage<T> {
-
-    protected final String name;
-
-    protected Storage(String name) {
-      super();
-      this.name = name;
-    }
-
-    protected String key(String... path) {
-      StringBuilder sb = null;
-      for (String s : path) {
-        if (sb == null) {
-          sb = new StringBuilder();
-        } else {
-          sb.append(".");
-        }
-        sb.append(s);
-      }
-      return sb.toString();
-    }
-
-    protected <K, V> V getUnique(Multimap<K, V> mm, K key) {
-      Collection<V> values = mm.get(key);
-      if (values.size() != 1) {
-        throw new ArrayIndexOutOfBoundsException(key + " expected to have exactly 1 value, got " + values.size() + "(" + values + ")");
-      }
-      return values.iterator().next();
-    }
-
-    protected abstract String getId(T t);
-
-    public String collection() {
-      return name + "s";
-    }
-
-    public String collectionKey(String parentKey) {
-      return parentKey == null ? collection() : key(parentKey, collection());
-    }
-
-    public String rootKey(String parentKey, String id) {
-      String relativeKey = key(name, id);
-      return parentKey == null ? relativeKey : key(parentKey, relativeKey);
-    }
-
-    public void saveAll(String parentKey, Multimap<String, String> store, List<T> c) {
-      for (T t : c) {
-        save(parentKey, store, t);
-      }
-    }
-
-    public void save(String parentKey, Multimap<String, String> store, T t) {
-      store.put(collectionKey(parentKey), getId(t));
-      saveContent(rootKey(parentKey, getId(t)), store, t);
-    }
-
-    public abstract void saveContent(String rootKey, Multimap<String, String> store, T t);
-
-    public abstract T load(String rootKey, String id, Multimap<String, String> store);
-
-    public List<T> loadAll(String parentKey, Multimap<String, String> store) {
-      Collection<String> ids = store.get(collectionKey(parentKey));
-      List<T> descriptors = new ArrayList<>(ids.size());
-      for (String id : ids) {
-        descriptors.add(load(rootKey(parentKey, id), id, store));
-      }
-      return descriptors;
-    }
-  }
-
-  public static final class AttributeStorage extends Storage<AttributeDescriptor> {
-
-    protected AttributeStorage() {
-      super("attribute");
-    }
-
-    @Override
-    protected String getId(AttributeDescriptor t) {
-      return t.name;
-    }
-
-    @Override
-    public void saveContent(String rootKey, Multimap<String, String> store, AttributeDescriptor t) {
-      store.put(key(rootKey, "value"), t.value);
-    }
-
-    @Override
-    public AttributeDescriptor load(String rootKey, String id, Multimap<String, String> store) {
-     return new AttributeDescriptor(id, getUnique(store, key(rootKey, "value")));
-    }
-
-  }
-
-  public static final class AnnotationStorage extends Storage<AnnotationDescriptor> {
-
-    private final AttributeStorage attributeStorage = new AttributeStorage();
-
-    protected AnnotationStorage() {
-      super("annotation");
-    }
-
-    @Override
-    protected String getId(AnnotationDescriptor t) {
-      return t.annotationType;
-    }
-
-    @Override
-    public void saveContent(String rootKey, Multimap<String, String> store, AnnotationDescriptor t) {
-      attributeStorage.saveAll(rootKey, store, t.attributes);
-    }
-
-    @Override
-    public AnnotationDescriptor load(String rootKey, String id, Multimap<String, String> store) {
-     return new AnnotationDescriptor(id, attributeStorage.loadAll(rootKey, store));
-    }
-
-  }
-
-  public static final class FieldStorage extends Storage<FieldDescriptor> {
-
-    private final AnnotationStorage annotationStorage = new AnnotationStorage();
-
-    protected FieldStorage() {
-      super("field");
-    }
-
-    @Override
-    protected String getId(FieldDescriptor t) {
-      return t.name;
-    }
-
-    @Override
-    public void saveContent(String rootKey, Multimap<String, String> store, FieldDescriptor t) {
-      store.put(key(rootKey, "descriptor"), t.descriptor);
-      annotationStorage.saveAll(rootKey, store, t.annotations);
-    }
-
-    @Override
-    public FieldDescriptor load(String rootKey, String id, Multimap<String, String> store) {
-     return new FieldDescriptor(id, getUnique(store, key(rootKey, "descriptor")), annotationStorage.loadAll(rootKey, store));
-    }
-
-  }
-
-  public static final class FunctionStorage extends Storage<FunctionDescriptor> {
-
-    private final AnnotationStorage annotationStorage = new AnnotationStorage();
-    private final FieldStorage fieldStorage = new FieldStorage();
-
-    protected FunctionStorage() {
-      super("function");
-    }
-
-    @Override
-    protected String getId(FunctionDescriptor t) {
-      return t.className;
-    }
-
-    @Override
-    public void saveContent(String rootKey, Multimap<String, String> store, FunctionDescriptor t) {
-      annotationStorage.saveAll(rootKey, store, t.annotations);
-      fieldStorage.saveAll(rootKey, store, t.fields);
-    }
-
-    @Override
-    public FunctionDescriptor load(String rootKey, String id, Multimap<String, String> store) {
-      return new FunctionDescriptor(
-        id,
-        annotationStorage.loadAll(rootKey, store),
-        fieldStorage.loadAll(rootKey, store)
-      );
-    }
-
-  }
-
   public static final class AttributeDescriptor {
     final String name;
     final String value;
-    public AttributeDescriptor(String name, String value) {
+    @JsonCreator
+    public AttributeDescriptor(
+        @JsonProperty("name")
+        String name,
+        @JsonProperty("value")
+        String value) {
       this.name = name;
       this.value = value;
     }
+    public String getName() {
+      return name;
+    }
+    public String getValue() {
+      return value;
+    }
     @Override
     public String toString() {
-      return "AttributeDescriptor [name=" + name + ", value=" + value + "]";
+      return "Attribute[" + name + "=" + value + "]";
     }
   }
 
   public static final class AnnotationDescriptor {
     final String annotationType;
     final List<AttributeDescriptor> attributes;
-    public AnnotationDescriptor(String annotationType, List<AttributeDescriptor> attributes) {
+    @JsonCreator
+    public AnnotationDescriptor(
+        @JsonProperty("annotationType")
+        String annotationType,
+        @JsonProperty("attributes")
+        List<AttributeDescriptor> attributes) {
       this.annotationType = annotationType;
       this.attributes = attributes;
     }
+    public String getAnnotationType() {
+      return annotationType;
+    }
+    public List<AttributeDescriptor> getAttributes() {
+      return attributes;
+    }
     @Override
     public String toString() {
-      return "AnnotationDescriptor [annotationType=" + annotationType + ", attributes=" + attributes + "]";
+      return "Annotation[type=" + annotationType + ", attributes=" + attributes + "]";
     }
   }
 
@@ -293,14 +145,30 @@ public final class FunctionResolver {
     final String name;
     final String descriptor;
     final List<AnnotationDescriptor> annotations;
-    public FieldDescriptor(String name, String descriptor, List<AnnotationDescriptor> annotations) {
+    @JsonCreator
+    public FieldDescriptor(
+        @JsonProperty("name")
+        String name,
+        @JsonProperty("descriptor")
+        String descriptor,
+        @JsonProperty("annotations")
+        List<AnnotationDescriptor> annotations) {
       this.name = name;
       this.descriptor = descriptor;
       this.annotations = annotations;
     }
+    public String getName() {
+      return name;
+    }
+    public String getDescriptor() {
+      return descriptor;
+    }
+    public List<AnnotationDescriptor> getAnnotations() {
+      return annotations;
+    }
     @Override
     public String toString() {
-      return "FieldDescriptor [name=" + name + ", descriptor=" + descriptor + ", annotations=" + annotations + "]";
+      return "Field[name=" + name + ", descriptor=" + descriptor + ", annotations=" + annotations + "]";
     }
   }
 
@@ -308,64 +176,165 @@ public final class FunctionResolver {
     final String className;
     final List<AnnotationDescriptor> annotations;
     final List<FieldDescriptor> fields;
-    public FunctionDescriptor(String className, List<AnnotationDescriptor> annotations, List<FieldDescriptor> fields) {
+    @JsonCreator
+    public FunctionDescriptor(
+        @JsonProperty("className")
+        String className,
+        @JsonProperty("annotations")
+        List<AnnotationDescriptor> annotations,
+        @JsonProperty("fields")
+        List<FieldDescriptor> fields) {
       this.className = className;
       this.annotations = Collections.unmodifiableList(annotations);
       this.fields = Collections.unmodifiableList(fields);
     }
+    public String getClassName() {
+      return className;
+    }
+    public List<AnnotationDescriptor> getAnnotations() {
+      return annotations;
+    }
+    public List<FieldDescriptor> getFields() {
+      return fields;
+    }
     @Override
     public String toString() {
-      return "FunctionDescriptor [className=" + className + ", annotations=" + annotations
+      return "Function [className=" + className + ", annotations=" + annotations
           + ", fields=" + fields + "]";
     }
   }
 
-
-  private static Collection<URL> getMarkedPaths() {
-    return PathScanner.forResource(CommonConstants.DRILL_JAR_MARKER_FILE_RESOURCE_PATHNAME, true);
-  }
-
-  private final List<FunctionDescriptor> functions;
-
-  FunctionResolver(List<FunctionDescriptor> functions) {
-    this.functions = Collections.unmodifiableList(functions);
-  }
-
-  private static Reflections scan() {
-    long t0 = System.currentTimeMillis();
-    try {
-      ConfigurationBuilder conf = new ConfigurationBuilder()
-          .setUrls(getMarkedPaths())
-          .setMetadataAdapter(new JavassistAdapter()) // FunctionScanner depends on this
-          .setScanners(FUNCTION_SCANNER);
-      return new Reflections(conf);
-    } finally {
-      long t1 = System.currentTimeMillis();
-      logger.debug("scanning took " + (t1 - t0) + "ms");
-      System.out.println("scanning took " + (t1 - t0) + "ms");
+  public static final class ScanResult {
+    final List<String> prescannedURLs;
+    final List<String> scannedURLs;
+    final List<FunctionDescriptor> functions;
+    @JsonCreator
+    public ScanResult(
+        @JsonProperty("prescannedURLs")
+        List<String> prescannedURLs,
+        @JsonProperty("scannedURLs")
+        List<String> scannedURLs,
+        @JsonProperty("functions")
+        List<FunctionDescriptor> functions) {
+      super();
+      this.prescannedURLs = prescannedURLs;
+      this.scannedURLs = scannedURLs;
+      this.functions = functions;
+    }
+    public List<String> getPrescannedURLs() {
+      return prescannedURLs;
+    }
+    public List<String> getScannedURLs() {
+      return scannedURLs;
+    }
+    public List<FunctionDescriptor> getFunctions() {
+      return functions;
+    }
+    @Override
+    public String toString() {
+      return "ScanResult [prescannedURLs=" + prescannedURLs + ", scannedURLs=" + scannedURLs + ", functions=" + functions
+          + "]";
+    }
+    public ScanResult merge(ScanResult other) {
+      final List<String> newPrescannedURLs = new ArrayList<>(prescannedURLs);
+      newPrescannedURLs.addAll(other.prescannedURLs);
+      final List<String> newScannedURLs = new ArrayList<>(scannedURLs);
+      newScannedURLs.addAll(other.scannedURLs);
+      final List<FunctionDescriptor> newFunctions = new ArrayList<>(functions);
+      newFunctions.addAll(other.functions);
+      return new ScanResult(newPrescannedURLs, newScannedURLs, newFunctions);
     }
   }
 
-  private static FunctionResolver fromStore(Store store) {
-    java.util.List<FunctionDescriptor> functions = new FunctionStorage().loadAll(null, store.get(FunctionScanner.class));
-    return new FunctionResolver(functions);
+  static Collection<URL> getMarkedPaths() {
+    return PathScanner.forResource(CommonConstants.DRILL_JAR_MARKER_FILE_RESOURCE_PATHNAME, true);
   }
 
-  public static FunctionResolver fromScan() {
-    return fromStore(scan().getStore());
+  static Collection<URL> getPrescannedPaths() {
+    return PathScanner.forResource(FUNCTION_REGISTRY_FILE, true);
   }
 
-  public static FunctionResolver fromClassPath() {
-    return fromStore(load().getStore());
+  static Collection<URL> getNonPrescannedMarkedPaths() {
+    Collection<URL> markedPaths = getMarkedPaths();
+    markedPaths.removeAll(getPrescannedPaths());
+    return markedPaths;
   }
 
-  private static void save(Reflections reflections, File reflectionsFile) {
-    scan().save(reflectionsFile.getAbsolutePath(), REFLECTIONS_SERIALIZER);
+  static ScanResult fromFullScan() {
+    return scan(getMarkedPaths());
   }
 
-  private static Reflections load() {
-    InputStream reflections = FunctionResolver.class.getClassLoader().getResourceAsStream(REFLECTION_FILE);
-    return REFLECTIONS_SERIALIZER.read(reflections);
+  private static List<String> mapToString(Collection<? extends Object> l) {
+    List<String> result = new ArrayList<>(l.size());
+    for (Object o : l) {
+      result.add(o.toString());
+    }
+    return result;
+  }
+
+  static ScanResult scan(Collection<URL> pathsToScan) {
+    long t0 = System.currentTimeMillis();
+    try {
+      FunctionResolver.FunctionScanner functionScanner = new FunctionResolver.FunctionScanner();
+      ConfigurationBuilder conf = new ConfigurationBuilder()
+          .setUrls(pathsToScan)
+          .setMetadataAdapter(new JavassistAdapter()) // FunctionScanner depends on this
+          .setScanners(functionScanner);
+      new Reflections(conf); // scans stuff, but don't use the funky storage layer
+      return new ScanResult(Collections.<String>emptyList(), mapToString(pathsToScan), functionScanner.functions);
+    } finally {
+      long t1 = System.currentTimeMillis();
+      logger.debug("scanning " + pathsToScan + " took " + (t1 - t0) + "ms");
+    }
+  }
+
+  public static ScanResult fromPrescan() {
+    ScanResult prescanned = load();
+    Set<String> prescannedURLs = new HashSet<>();
+    if (prescanned != null) {
+      prescannedURLs.addAll(prescanned.scannedURLs);
+      prescannedURLs.addAll(prescanned.prescannedURLs);
+      prescanned = new ScanResult(
+          new ArrayList<>(prescannedURLs),
+          Collections.<String>emptyList(),
+          prescanned.functions);
+    }
+    Collection<URL> toScan = getNonPrescannedMarkedPaths();
+    for (Iterator<URL> iterator = toScan.iterator(); iterator.hasNext();) {
+      URL url = iterator.next();
+      if (prescannedURLs.contains(url.toString())) {
+        iterator.remove();
+      }
+    }
+    ScanResult others = scan(toScan);
+    ScanResult merged = prescanned == null ? others : prescanned.merge(others);
+    return merged;
+  }
+
+  private static void save(ScanResult scanResult, File file) {
+    try {
+      writer.writeValue(file, scanResult);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static ScanResult load() {
+    Set<URL> preScanned = PathScanner.forResource(FUNCTION_REGISTRY_FILE, false);
+    ScanResult result = null;
+    for (URL u : preScanned) {
+      try (InputStream reflections = u.openStream()) {
+        ScanResult ref = reader.readValue(reflections);
+        if (result == null) {
+          result = ref;
+        } else {
+          result = result.merge(ref);
+        }
+      } catch (IOException e) {
+        throw new RuntimeException("can't read function registry at " + u, e);
+      }
+    }
+    return result;
   }
 
   public static void main(String[] args) {
@@ -373,17 +342,12 @@ public final class FunctionResolver {
       throw new IllegalArgumentException("Usage: java {cp} " + PathScanner.class.getName() + " path/to/generate");
     }
     String basePath = args[0];
-    File reflectionsFile = new File(basePath, REFLECTION_FILE);
+    File reflectionsFile = new File(basePath, FUNCTION_REGISTRY_FILE);
     File dir = reflectionsFile.getParentFile();
     if ((!dir.exists() && !dir.mkdirs()) || !dir.isDirectory()) {
       throw new IllegalArgumentException("could not create dir " + dir.getAbsolutePath());
     }
-    save(scan(), reflectionsFile);
+    save(fromFullScan(), reflectionsFile);
   }
-
-  public List<FunctionDescriptor> getFunctions() {
-    return functions;
-  }
-
 
 }
--- exec/java-exec/src/test/java/org/apache/drill/common/util/TestFunctionResolver.java
+++ exec/java-exec/src/test/java/org/apache/drill/common/util/TestFunctionResolver.java
@@ -1,14 +1,55 @@
 package org.apache.drill.common.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.drill.common.util.FunctionResolver.FieldDescriptor;
+import org.apache.drill.common.util.FunctionResolver.FunctionDescriptor;
+import org.apache.drill.common.util.FunctionResolver.ScanResult;
+import org.apache.drill.exec.expr.annotations.FunctionTemplate;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestFunctionResolver {
 
+  private void assertIsSuffixedBy(Collection<String> c, String... suffixes) {
+    assertEquals("expected " + Arrays.toString(suffixes) + " to have same size as" + c, suffixes.length, c.size());
+    outer: for (String suffix : suffixes) {
+      for (String item : c) {
+        if (item.endsWith(suffix)) {
+          continue outer;
+        }
+      }
+      Assert.fail("could not find suffix " + suffix + " in " + c);
+    }
+  }
+
   @Test
-  public void test() {
-    System.out.println(FunctionResolver.fromClassPath().getFunctions());
+  public void test() throws Exception {
+    ScanResult result = FunctionResolver.fromPrescan();
+    assertIsSuffixedBy(result.getPrescannedURLs(),
+        "/exec/java-exec/target/classes/",
+        "/common/target/classes/",
+        "/common/target/test-classes/");
+    assertIsSuffixedBy(result.getScannedURLs(), "/exec/java-exec/target/test-classes/");
+    List<FunctionDescriptor> functions = result.getFunctions();
+    for (FunctionDescriptor function : functions) {
+      Class<?> c = Class.forName(function.className, false, this.getClass().getClassLoader());
+      FunctionTemplate annotation = c.getAnnotation(FunctionTemplate.class);
+      Field[] fields = c.getDeclaredFields();
+      Assert.assertEquals("fields count for " + function, fields.length, function.fields.size());
+      for (int i = 0; i < fields.length; i++) {
+        FieldDescriptor fieldDescriptor = function.fields.get(i);
+        Field field = fields[i];
+        Assert.assertEquals("Class fields:\n" + Arrays.toString(fields) + "\n != \nDescriptor fields:\n" + function.fields, field.getName(), fieldDescriptor.name);
+      }
+//      System.out.println(annotation);
+    }
+//    System.out.println(functions);
   }
 
 }
