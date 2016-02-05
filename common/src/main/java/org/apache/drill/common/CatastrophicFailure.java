apache/drill

DRILL-3581: Upgrade to Guava 18.0
- Replace Stopwatch constructors with .createStarted() or .createUnstarted()
- Stop using InputSupplier and Closeables.closeQuietly
- Clean up quiet closes to log or (preferably) propagate.
- Add log4j to enforcer exclusions.
- Update HBaseTestSuite to add patching of Closeables.closeQuietly() and Stopwatch legacy methods. Only needed when running HBaseMiniCluster.
- Remove log4j from HBase's pom to provide exception logging.
- Remove log4j from Hive's shaded pom.
- Update Catastrophic failures to use the same pattern to ensure reporting.
- Update test framework to avoid trying IPv6 resolution. (This removes 90s pause from HBase startup in my tests)

common/src/main/java/org/apache/drill/common/CatastrophicFailure.java
@@ -1,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.drill.common;
+
+import java.io.PrintStream;
+
+
+public class CatastrophicFailure {
+  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CatastrophicFailure.class);
+
+  private CatastrophicFailure() {
+  }
+
+  /**
+   * Exit the VM as we hit a catastrophic failure.
+   * @param e
+   *          The Throwable that occurred
+   * @param name
+   *          A descriptive message
+   * @param code
+   *          An error code to exit the VM with.
+   */
+  public static void exit(Throwable e, String message, int code) {
+    logger.error("Catastrophic Failure Occurred, exiting. Information message: {}", message, e);
+
+    final PrintStream out = ("true".equals(System.getProperty("drill.catastrpohic_to_standard_out", "true"))) ? System.out
+        : System.err;
+    out.println("Catastrophic failure occurred. Exiting. Information follows: " + message);
+    e.printStackTrace(out);
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e2) {
+    }
+    System.exit(code);
+  }
+}

contrib/storage-hbase/src/test/java/org/apache/drill/hbase/BaseHBaseTest.java
@@ -46,8 +46,11 @@ public class BaseHBaseTest extends BaseTestQuery {
 
   protected static HBaseStoragePluginConfig storagePluginConfig;
 
+
   @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
+  public static void setupDefaultTestCluster() throws Exception {
+    GauavaPatcher.patch();
+
     /*
      * Change the following to HBaseTestsSuite.configure(false, true)
      * if you want to test against an externally running HBase cluster.
@@ -55,12 +58,15 @@ public class BaseHBaseTest extends BaseTestQuery {
     HBaseTestsSuite.configure(true, true);
     HBaseTestsSuite.initCluster();
 
+    BaseTestQuery.setupDefaultTestCluster();
+
     final StoragePluginRegistry pluginRegistry = getDrillbitContext().getStorage();
     storagePlugin = (HBaseStoragePlugin) pluginRegistry.getPlugin(HBASE_STORAGE_PLUGIN_NAME);
     storagePluginConfig = storagePlugin.getConfig();
     storagePluginConfig.setEnabled(true);
     storagePluginConfig.setZookeeperPort(HBaseTestsSuite.getZookeeperPort());
     pluginRegistry.createOrUpdate(HBASE_STORAGE_PLUGIN_NAME, storagePluginConfig, true);
+
   }
 
   @AfterClass
@@ -101,5 +107,7 @@ public class BaseHBaseTest extends BaseTestQuery {
   protected String canonizeHBaseSQL(String sql) {
     return sql.replace("[TABLE_NAME]", HBaseTestsSuite.TEST_TABLE_1);
   }
+
+
 
 }

contrib/storage-hbase/src/test/java/org/apache/drill/hbase/HBaseTestsSuite.java
@@ -81,6 +81,8 @@ public class HBaseTestsSuite {
 
   @BeforeClass
   public static void initCluster() throws Exception {
+    GauavaPatcher.patch();
+
     if (initCount.get() == 0) {
       synchronized (HBaseTestsSuite.class) {
         if (initCount.get() == 0) {

jacques-n/incubator-drill

common/src/main/java/org/apache/drill/common/CatastrophicFailure.java
@@ -1,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.drill.common;
+
+import java.io.PrintStream;
+
+
+public class CatastrophicFailure {
+  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CatastrophicFailure.class);
+
+  private CatastrophicFailure() {
+  }
+
+  /**
+   * Exit the VM as we hit a catastrophic failure.
+   * @param e
+   *          The Throwable that occurred
+   * @param name
+   *          A descriptive message
+   * @param code
+   *          An error code to exit the JVM with.
+   */
+  public static void exit(Throwable e, String message, int code) {
+    logger.error("Catastrophic Failure Occurred, exiting. Information message: {}", message, e);
+
+    final PrintStream out = ("true".equals(System.getProperty("drill.catastrophic_to_standard_out", "true"))) ? System.out
+        : System.err;
+    out.println("Catastrophic failure occurred. Exiting. Information follows: " + message);
+    e.printStackTrace(out);
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e2) {
+    }
+    System.exit(code);
+  }
+}

contrib/storage-hbase/src/test/java/org/apache/drill/hbase/BaseHBaseTest.java
@@ -46,8 +46,11 @@ public class BaseHBaseTest extends BaseTestQuery {
 
   protected static HBaseStoragePluginConfig storagePluginConfig;
 
+
   @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
+  public static void setupDefaultTestCluster() throws Exception {
+    GuavaPatcher.patch();
+
     /*
      * Change the following to HBaseTestsSuite.configure(false, true)
      * if you want to test against an externally running HBase cluster.
@@ -55,12 +58,15 @@ public class BaseHBaseTest extends BaseTestQuery {
     HBaseTestsSuite.configure(true, true);
     HBaseTestsSuite.initCluster();
 
+    BaseTestQuery.setupDefaultTestCluster();
+
     final StoragePluginRegistry pluginRegistry = getDrillbitContext().getStorage();
     storagePlugin = (HBaseStoragePlugin) pluginRegistry.getPlugin(HBASE_STORAGE_PLUGIN_NAME);
     storagePluginConfig = storagePlugin.getConfig();
     storagePluginConfig.setEnabled(true);
     storagePluginConfig.setZookeeperPort(HBaseTestsSuite.getZookeeperPort());
     pluginRegistry.createOrUpdate(HBASE_STORAGE_PLUGIN_NAME, storagePluginConfig, true);
+
   }
 
   @AfterClass
@@ -101,5 +107,7 @@ public class BaseHBaseTest extends BaseTestQuery {
   protected String canonizeHBaseSQL(String sql) {
     return sql.replace("[TABLE_NAME]", HBaseTestsSuite.TEST_TABLE_1);
   }
+
+
 
 }

contrib/storage-hbase/src/test/java/org/apache/drill/hbase/HBaseTestsSuite.java
@@ -81,6 +81,8 @@ public class HBaseTestsSuite {
 
   @BeforeClass
   public static void initCluster() throws Exception {
+    GuavaPatcher.patch();
+
     if (initCount.get() == 0) {
       synchronized (HBaseTestsSuite.class) {
         if (initCount.get() == 0) {